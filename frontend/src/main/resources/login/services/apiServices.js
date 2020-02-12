5731165ee926fe0c3f5fb2e4e1534cbe9484bee3
ahshahid <ashahid@snappydata.io>
2020-02-12 12:47:38 -0800

    initial changes

--- frontend/src/main/resources/login/services/apiServices.js
+++ frontend/src/main/resources/login/services/apiServices.js
@@ -2,7 +2,7 @@ app.factory('ApiFactory', ['$resource',
 	function ($resource) {
         return {
             login: $resource('http://35.202.5.109:9090/api/login'),
-            schema: $resource('http://35.202.5.109:9090/api/login')
+            schema: $resource('http://35.202.5.109:9090/api/schema')
         }
 		
 	}
