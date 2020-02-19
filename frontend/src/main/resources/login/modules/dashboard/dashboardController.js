5affa69352c5bcf36b5eb052155e0251f458b983
Rohinee-Misal <rohineemisal92@gmail.om>
2020-02-20 00:23:17 +0530

    implemented the ng-table in the app

--- frontend/src/main/resources/login/modules/dashboard/dashboardController.js
+++ frontend/src/main/resources/login/modules/dashboard/dashboardController.js
@@ -1,14 +1,29 @@
-app.controller('dashboardController', ['$scope', '$http', 'ApiFactory', '$stateParams', function($scope, $http, ApiFactory, $stateParams) {
+app.controller('dashboardController', ['$scope', '$http', 'ApiFactory', '$stateParams', 'NgTableParams', function($scope, $http, ApiFactory, $stateParams, NgTableParams) {
 
-        $scope.tableName = $stateParams.table;
+    $scope.tableName = $stateParams.table;
         $scope.schemaCols = {};
-
+        $scope.schemaRows = {};
         $scope.getData = function() {
             ApiFactory.schema.save({
-                tablename: $scope.tableName
+                tablename: $stateParams.table
             }, function (response) {
                 $scope.schemaCols = response.schema.columns;
+               /*  $scope.schemaCols =  new NgTableParams({page: 1,
+                    count: 1,
+                    filter: {},
+                    sorting: {}}, {dataset: data}); */
             });
-            }
+            ApiFactory.getRows.save({
+                tablename: $stateParams.table
+            }, function (response) {
+                var data = response.rows;
+                console.log(response.rows);
+                $scope.schemaRows =  new NgTableParams({page: 1,
+                    count: 2,
+                    filter: {},
+                    sorting: {}}, {dataset: data});
+            
+            });
+        }
 
 }])
